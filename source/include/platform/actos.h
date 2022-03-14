124b710a8856e45f6685056ccd6a0c1d9dddd6e7
tjysdsg <tjy1018543509@163.com>
2022-03-15 00:36:14 +0800

    Optmize actos.h

--- source/include/platform/actos.h
+++ source/include/platform/actos.h
@@ -1,31 +1,22 @@
 #ifndef __ACTOS_H__
 #define __ACTOS_H__
 
-/// ======================= tos custom options =======================
-#include <stdint.h>
-#define INT8 int8_t
-#define UINT8 uint8_t
-#define INT16 int16_t
-#define UINT16 uint16_t
-#define INT32 int32_t
-#define UINT32 uint32_t
-#define INT64 int64_t
-#define UINT64 uint64_t
-#define BITS_PER_LONG 32
-#define BOOLEAN int
+#include "acgcc.h"
+#include "acgccex.h"
+#include <string.h>
+#include <kernel.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <sched.h>
+#include <atomic.h>
+#include <spinlock_types.h>
 
-#define ACPI_USE_NATIVE_DIVIDE
-/// ==================================================================
-
-/* Common (in-kernel/user-space) ACPICA configuration */
-
-#define ACPI_USE_SYSTEM_CLIBRARY
 #define ACPI_USE_DO_WHILE_0
+#define ACPI_USE_STANDARD_HEADERS
+#define ACPI_USE_SYSTEM_CLIBRARY
 
-#ifdef __KERNEL__
-
-#define ACPI_USE_SYSTEM_INTTYPES
-#define ACPI_USE_GPE_POLLING
+#define ACPI_DEBUG_OUTPUT
+#define CONFIG_ACPI_DEBUG
 
 /* Kernel specific ACPICA configuration */
 
@@ -41,85 +32,24 @@
 #define ACPI_MUTEX_DEBUG
 #endif
 
-#include <string.h>
-#include <kernel.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <sched.h>
-#include <atomic.h>
-#include <spinlock_types.h>
-
-#ifdef CONFIG_ACPI
-#include <asm/acenv.h>
-#endif
-
 #define ACPI_INIT_FUNCTION
 
 #ifndef CONFIG_ACPI
+
+/* Generating stubs for configurable ACPICA functions */
+
+#undef ACPI_DEBUG_OUTPUT
 
 #endif /* CONFIG_ACPI */
 
 /* Host-dependent types and defines for in-kernel ACPICA */
 
-#define ACPI_MACHINE_WIDTH          BITS_PER_LONG
-#define ACPI_USE_NATIVE_MATH64
-#define ACPI_EXPORT_SYMBOL(symbol)
-#define strtoul                     simple_strtoul
-
-#define ACPI_CACHE_T                ACPI_MEMORY_LIST
-#define ACPI_USE_LOCAL_CACHE        1
-
-#define ACPI_SPINLOCK               spinlock_t *
-#define ACPI_CPU_FLAGS              unsigned long
-
-#define ACPI_UINTPTR_T              uintptr_t
-
-#define ACPI_TO_INTEGER(p)          ((uintptr_t)(p))
-#define ACPI_OFFSET(d, f)           offsetof(d, f)
-
-/* Use native linux version of AcpiOsAllocateZeroed */
-
-#define USE_NATIVE_ALLOCATE_ZEROED
-
-#define ACPI_MSG_ERROR          "ACPI Error: "
-#define ACPI_MSG_EXCEPTION      "ACPI Exception: "
-#define ACPI_MSG_WARNING        "ACPI Warning: "
-#define ACPI_MSG_INFO           "ACPI: "
-
-#define ACPI_MSG_BIOS_ERROR     "ACPI BIOS Error (bug): "
-#define ACPI_MSG_BIOS_WARNING   "ACPI BIOS Warning (bug): "
-
-/*
- * Linux wants to use designated initializers for function pointer structs.
- */
-#define ACPI_STRUCT_INIT(field, value)  .field = value
-
-#else /* !__KERNEL__ */
-
-#define ACPI_USE_STANDARD_HEADERS
-
-#ifdef ACPI_USE_STANDARD_HEADERS
-#include <unistd.h>
-#endif
-
-/* Define/disable kernel-specific declarators */
-
-#ifndef __init
-#define __init
-#endif
-#ifndef __iomem
-#define __iomem
-#endif
-
-/* Host-dependent types and defines for user-space ACPICA */
-
 #define ACPI_FLUSH_CPU_CACHE()
 #define ACPI_CAST_PTHREAD_T(Pthread) ((ACPI_THREAD_ID) (Pthread))
 
-#if defined(__ia64__)    || (defined(__x86_64__) && !defined(__ILP32__)) ||\
+#if defined(__ia64__)    || defined(__x86_64__) ||\
     defined(__aarch64__) || defined(__PPC64__) ||\
-    defined(__s390x__) ||\
-    (defined(__riscv) && (defined(__LP64__) || defined(_LP64)))
+    defined(__s390x__)
 #define ACPI_MACHINE_WIDTH          64
 #define COMPILER_DEPENDENT_INT64    long
 #define COMPILER_DEPENDENT_UINT64   unsigned long
@@ -135,6 +65,22 @@
 #define __cdecl
 #endif
 
-#endif /* __KERNEL__ */
+#define ACPI_CACHE_T                ACPI_MEMORY_LIST
+#define ACPI_USE_LOCAL_CACHE        1
+
+#define ACPI_USE_NATIVE_MATH64
+#define ACPI_EXPORT_SYMBOL(symbol)  EXPORT_SYMBOL(symbol);
+#define strtoul                     simple_strtoul
+
+#define ACPI_SPINLOCK               spinlock_t *
+#define ACPI_CPU_FLAGS              unsigned long
+
+#define ACPI_MSG_ERROR          "ACPI Error: "
+#define ACPI_MSG_EXCEPTION      "ACPI Exception: "
+#define ACPI_MSG_WARNING        "ACPI Warning: "
+#define ACPI_MSG_INFO           "ACPI: "
+
+#define ACPI_MSG_BIOS_ERROR     "ACPI BIOS Error (bug): "
+#define ACPI_MSG_BIOS_WARNING   "ACPI BIOS Warning (bug): "
 
 #endif /* __ACTOS_H__ */
